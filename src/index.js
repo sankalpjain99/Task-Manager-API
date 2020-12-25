4c5aaf8a34c24e147c1a2d404722eb39c4eb5cea
sankalpjain99 <sankalpjain99@gmail.com>
2020-12-25 14:31:02 +0530

    Added CORS npm package

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 require("./db/mongoose.js");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
@@ -6,6 +7,7 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT;
 
+app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
